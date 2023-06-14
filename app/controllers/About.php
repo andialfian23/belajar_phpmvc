f83da63efba2db4df56aee67ff56e04557cea12d
andialfian23 <andialfi90@gmail.com>
2023-06-14 19:45:10 +0700

    update struktur file view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -3,7 +3,6 @@ class About extends Controller
 {
   public function index($nama = 'Andi Alfian', $pekerjaan = 'Programmer', $umur = 23)
   {
-    // echo "Halo, nama saya $nama, pekerjaan $pekerjaan. saya berumur $umur tahun";
     $data = [
       'nama' => $nama,
       'pekerjaan' => $pekerjaan,
@@ -11,10 +10,5 @@ class About extends Controller
       'title' => 'Halaman About'
     ];
     $this->view('about/index', $data);
-  }
-  public function page()
-  {
-    $data['title'] = 'Pages';
-    $this->view('about/page');
   }
 }
